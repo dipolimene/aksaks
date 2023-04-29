8c58162a80195673846126686a7fff3ba0dd4fec
Oladipupo Olimene <dipotope@yahoo.com>
2023-04-29 22:07:44 +0200

    update tf files

--- kv.tf
+++ kv.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_key_vault" "kv" {
-  name                       = "${local.prefix}-kvo"
+  name                       = "${local.prefix}-vk"
   location                   = azurerm_resource_group.resource_group.location
   resource_group_name        = azurerm_resource_group.resource_group.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
